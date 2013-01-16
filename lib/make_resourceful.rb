1fad16a3bf1bcecdaff54220df00c3fa766d210e
Benjamin A Wagaman <benjamin.a.wagaman@chase.com>
2013-01-16 10:34:40 -0500

    Dropping back to the pre-Rails 3 way of initializing

--- lib/make_resourceful.rb
+++ lib/make_resourceful.rb
@@ -1,7 +1,2 @@
-
-class MakeResourcefulTie < Rails::Railtie
-  initializer "my_railtie.configure_rails_initialization" do
-    require_relative 'resourceful/maker'
-    ActionController::Base.extend Resourceful::Maker
-  end
-end
+require 'resourceful/maker'
+ActionController::Base.extend Resourceful::Maker
